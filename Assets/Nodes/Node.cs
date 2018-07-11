b7b7bf7417369c34eadd78337edaef6f640c2bbf
ClinkMakesGames <32272073+ClinkMakesGames@users.noreply.github.com>
2018-07-11 17:53:57 +0100

    Added rendering of nodes

---
+++ Assets/Nodes/Node.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Node : MonoBehaviour
+{
+    public string name;
+    public Field[] inputs;
+    public Field[] outputs;
+
+    [Space]
+
+    public GameObject inputFieldPrefab;
+    public GameObject outputFieldPrefab;
+    public Transform fieldParent;
+
+    [Space]
+
+    public float fieldSpacing;
+
+    public void Start()
+    {
+        GetComponentInChildren<TextMeshPro>().text = name;
+
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            GameObject go = Instantiate(inputFieldPrefab);
+            go.transform.parent = fieldParent;
+            go.transform.localPosition = new Vector3(0, fieldSpacing * i, 0);
+            go.GetComponentInChildren<TextMeshPro>().text = inputs[i].name + " (" + inputs[i].type + ")";
+        }
+
+        for (int i = 0; i < outputs.Length; i++)
+        {
+            GameObject go = Instantiate(outputFieldPrefab);
+            go.transform.parent = fieldParent;
+            go.transform.localPosition = new Vector3(0, fieldSpacing * i, 0);
+            go.GetComponentInChildren<TextMeshPro>().text = "(" + outputs[i].type + ") " + outputs[i].name;
+        }
+    }
+}

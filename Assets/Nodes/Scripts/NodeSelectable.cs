fcf14b2c2766f934ebd635c34fd898307af3dca2
ClinkMakesGames <32272073+ClinkMakesGames@users.noreply.github.com>
2018-07-20 09:41:42 +0100

    Made nodes selectable and deleteable

---
+++ Assets/Nodes/Scripts/NodeSelectable.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeSelectable : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject background_selected;
+
+    [Space]
+
+    [SerializeField]
+    private bool deleteable;
+
+    private bool selected = false;
+    private bool mouseOver;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            selected = mouseOver;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            selected = false;
+        }
+
+        background_selected.SetActive(selected);
+
+        if (selected)
+        {
+            if (deleteable && Input.GetKeyDown(KeyCode.Delete))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseOver = true;
+    }
+
+    private void OnMouseExit()
+    {
+        mouseOver = false;
+    }
+}

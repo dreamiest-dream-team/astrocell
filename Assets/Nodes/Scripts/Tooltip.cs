eb354c2cd7d5ff439cf4fc1e64cec8ae7f045c77
MininMobile <miningamer@gmail.com>
2018-08-13 00:20:13 +0100

    begin work on tooltips

---
+++ Assets/Nodes/Scripts/Tooltip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tooltip : MonoBehaviour
+{
+	[SerializeField]
+	private Camera cam;
+	[SerializeField]
+	private GameObject toolTipHost;
+	[SerializeField]
+	private bool description;
+
+	private bool mouseOver = false;
+
+	void Update ()
+	{
+		if (mouseOver)
+			toolTipHost.transform.position = cam.ScreenToWorldPoint(Input.mousePosition);
+
+		toolTipHost.SetActive(mouseOver);
+	}
+}
